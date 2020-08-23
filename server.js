b4d76e03aaacbf78583de744fe8634fdf00ecbf6
andyleeking9191 <andyleeking9191@users.noreply.github.com>
2020-08-22 23:57:00 -0400

    Added routes to query from the database

--- server.js
+++ server.js
@@ -5,6 +5,26 @@ const pool = require('./db');
 
 app.use(cors());
 app.use(express.json());
-app.listen(5000, () => {
-    console.log('The server is initialized on port 5000!')
+
+app.post('/todos', async (req, res) => {
+    try {
+        const { description } = req.body;
+        const newTodo = await pool.query('INSERT INTO todo (description) VALUES($1) RETURNING *',
+        [description]
+        );
+        res.json(newTodo.rows[0])
+    } catch (err) {
+        console.error(err.message);
+    }
 });
+
+app.get('/todos', async (req, res) => {
+    try {
+        const allTodos = await pool.query('SELECT * FROM todo');
+        res.json(allTodos.rows)
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+app.listen(5000, () => {console.log('The server is initialized on port 5000!')});
