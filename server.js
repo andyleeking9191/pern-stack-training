f86a7e5abfe40c73e936051994e5daf86b8c117b
andyleeking9191 <andyleeking9191@users.noreply.github.com>
2020-08-23 22:08:44 -0400

    Added crud operations to the server

--- server.js
+++ server.js
@@ -6,25 +6,67 @@ const pool = require('./db');
 app.use(cors());
 app.use(express.json());
 
+// INSERT NEW TODO
 app.post('/todos', async (req, res) => {
-    try {
-        const { description } = req.body;
-        const newTodo = await pool.query('INSERT INTO todo (description) VALUES($1) RETURNING *',
-        [description]
-        );
-        res.json(newTodo.rows[0])
-    } catch (err) {
-        console.error(err.message);
-    }
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      'INSERT INTO todo (description) VALUES($1) RETURNING *',
+      [description]
+    );
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
+// SELECT ALL TODOS
 app.get('/todos', async (req, res) => {
+  try {
+    const allTodos = await pool.query('SELECT * FROM todo');
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// SELECT SPECIFIC TODO BY ID
+app.get('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query('SELECT * FROM todo WHERE todo_id = $1', [
+      id
+    ]);
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// UPDATE  TODO BY ID
+app.put('/todos/:id', async (req, res) => {
     try {
-        const allTodos = await pool.query('SELECT * FROM todo');
-        res.json(allTodos.rows)
+        const { id } = req.params;
+        const { description } = req.body;
+        await pool.query('UPDATE todo SET description = $1 WHERE todo_id = $2', [description, id]);
+        res.json('Todo updated!')
     } catch (err) {
-        console.error(err.message);
+        console.error(err.message)
     }
 })
 
-app.listen(5000, () => {console.log('The server is initialized on port 5000!')});
+// DELETE TODO BY ID
+app.delete('/todos/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        await pool.query('DELETE FROM todo WHERE todo_id = $1', [id])
+        res.json('Todo deleted!')
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+
+app.listen(5000, () => {
+  console.log('The server is initialized on port 5000!');
+});
